55336f0fe7ed967992c6418495bf3acda20f074b
yobggi <formon0212@gmail.com>
2024-05-21 10:41:51 +0900

    [feat]firebase 수정

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,11 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+  apiKey: "AIzaSyB2GGju-lQ2RVRia6U-G_tfioJ308GMhHc",
+  authDomain: "blog-3e265.firebaseapp.com",
+  projectId: "blog-3e265",
+  storageBucket: "blog-3e265.appspot.com",
+  messagingSenderId: "978516629560",
+  appId: "1:978516629560:web:6c75ac97f2a25d817caecd",
+  measurementId: "G-07NHF3QE4L",
 };
 
 firebase.initializeApp(firebaseConfig);
